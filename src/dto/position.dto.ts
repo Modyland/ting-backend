116927ecdaef48acc41387ad99f094b591b33e58
gtrrpc2001 <cbdgjs3548@naver.com>
2024-04-12 16:39:22 +0900

    dto update

--- src/dto/position.dto.ts
+++ src/dto/position.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform, Type } from "class-transformer";
 import { IsArray, IsNumber,IsOptional,IsString } from "class-validator";
 import { Double,Int32 } from "typeorm";
@@ -6,23 +7,29 @@ export class positionDTO{
     
     @IsString()
     @IsOptional()
+    @ApiProperty({type:String, description:'post 구분 값'})
     readonly kind:string;
 
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly id:string;
 
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly writetime:string;   
 
     @IsString()
     @IsOptional()
+    @ApiProperty({type:Number, description:''})
     readonly latitude:number;
 
     @IsArray()
     @IsOptional()    
+    @ApiProperty({type:Number, description:''})
     readonly longitude:number;    
 
     @IsString()
     @IsOptional()
+    @ApiProperty({type:String, description:''})
     readonly address:string;
 }
