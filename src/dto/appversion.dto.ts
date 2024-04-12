0319fd6336fc4b008adf749489832e0d528db9c2
gtrrpc2001 <cbdgjs3548@naver.com>
2024-04-12 16:33:11 +0900

    swagger dto

--- src/dto/appversion.dto.ts
+++ src/dto/appversion.dto.ts
@@ -1,11 +1,13 @@
 import { IsNumber,IsOptional,IsString } from "class-validator";
-
+import { ApiProperty } from '@nestjs/swagger';
 
 export class appversionDTO{   
-    @IsString()    
+    @IsString()
+    @ApiProperty({type:String, description:''})
     readonly admin:string;
 
     @IsOptional()    
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly appversion:string;
 }
--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber,IsOptional,IsString,IsArray } from "class-validator";
 import { Double,Int32 } from "typeorm";
 
@@ -5,60 +6,75 @@ export class userDTO{
     
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:'post 구분 값'})
     readonly kind: string;
     
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly id:string;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly pwd:string;
     
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly phone:string;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly birth:string;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly gender:string;    
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly signupdate:string;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     readonly writetime:string;
 
     @IsOptional()
     @IsNumber()
+    @ApiProperty({type:Number, description:''})
     readonly pause:number;
 
     @IsOptional()
     @IsArray()
-    readonly profile:number[];
+    @ApiProperty({type:Buffer, description:''})
+    readonly profile:Buffer;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:'닉네임'})
     readonly aka:string;
 
     @IsOptional()
     @IsNumber()
+    @ApiProperty({type:String, description:''})
     readonly guard:string;
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     access_token:string;   
     
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:''})
     refresh_token:string; 
 
     @IsOptional()
     @IsString()
+    @ApiProperty({type:String, description:'firebase'})
     alarm_token:string;  
 }
